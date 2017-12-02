3ebc70604b4d41201b4bfca27b63af6cba0c1125
SeanNash <c00217019@itcarlow.ie>
2017-12-02 11:29:24 +0000

    Working Base

--- 2DCollision/main.cpp
+++ 2DCollision/main.cpp
@@ -3,7 +3,9 @@
 #define TINYC2_IMPL
 #include <tinyc2.h>
 #include <AnimatedSprite.h>
+#include <GameObject.h>
 #include <Player.h>
+#include <NPC.h>
 #include <Input.h>
 #include <Debug.h>
 
@@ -14,61 +16,120 @@ int main()
 	// Create the main window
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-	// Load a sprite to display
-	sf::Texture sprite_sheet;
-	if (!sprite_sheet.loadFromFile("assets\\grid.png")) {
+	// Load a NPC's sprites to display
+	sf::Texture npc_texture;
+	if (!npc_texture.loadFromFile("assets\\grid.png")) {
 		DEBUG_MSG("Failed to load file");
 		return EXIT_FAILURE;
 	}
 
 	// Load a mouse texture to display
-	sf::Texture mouse_texture;
-	if (!mouse_texture.loadFromFile("assets\\mouse.png")) {
+	sf::Texture player_texture;
+	if (!player_texture.loadFromFile("assets\\player.png")) {
 		DEBUG_MSG("Failed to load file");
 		return EXIT_FAILURE;
 	}
 
-	// Setup a mouse Sprite
-	sf::Sprite mouse;
-	mouse.setTexture(mouse_texture);
-
-	//Setup mouse AABB
-	c2AABB aabb_mouse;
-	aabb_mouse.min = c2V(mouse.getPosition().x, mouse.getPosition().y);
-	aabb_mouse.max = c2V(mouse.getGlobalBounds().width, mouse.getGlobalBounds().width);
+	// Setup NPC's Default Animated Sprite
+	AnimatedSprite npc_animated_sprite(npc_texture);
+	npc_animated_sprite.addFrame(sf::IntRect(3, 3, 84, 84));
+	npc_animated_sprite.addFrame(sf::IntRect(88, 3, 84, 84));
+	npc_animated_sprite.addFrame(sf::IntRect(173, 3, 84, 84));
+	npc_animated_sprite.addFrame(sf::IntRect(258, 3, 84, 84));
+	npc_animated_sprite.addFrame(sf::IntRect(343, 3, 84, 84));
+	npc_animated_sprite.addFrame(sf::IntRect(428, 3, 84, 84));
 
 	// Setup Players Default Animated Sprite
-	AnimatedSprite animated_sprite(sprite_sheet);
-	animated_sprite.addFrame(sf::IntRect(3, 3, 84, 84));
-	animated_sprite.addFrame(sf::IntRect(88, 3, 84, 84));
-	animated_sprite.addFrame(sf::IntRect(173, 3, 84, 84));
-	animated_sprite.addFrame(sf::IntRect(258, 3, 84, 84));
-	animated_sprite.addFrame(sf::IntRect(343, 3, 84, 84));
-	animated_sprite.addFrame(sf::IntRect(428, 3, 84, 84));
+	AnimatedSprite player_animated_sprite(player_texture);
+	player_animated_sprite.addFrame(sf::IntRect(3, 3, 84, 84));
+	player_animated_sprite.addFrame(sf::IntRect(88, 3, 84, 84));
+	player_animated_sprite.addFrame(sf::IntRect(173, 3, 84, 84));
+	player_animated_sprite.addFrame(sf::IntRect(258, 3, 84, 84));
+	player_animated_sprite.addFrame(sf::IntRect(343, 3, 84, 84));
+	player_animated_sprite.addFrame(sf::IntRect(428, 3, 84, 84));
 
-	// Setup Players AABB
-	c2AABB aabb_player;
-	aabb_player.min = c2V(animated_sprite.getPosition().x, animated_sprite.getPosition().y);
-	aabb_player.max = c2V(animated_sprite.getGlobalBounds().width / animated_sprite.getFrames().size(), 
-		animated_sprite.getGlobalBounds().height / animated_sprite.getFrames().size());
-
+	// Setup the NPC
+	GameObject &npc = NPC(npc_animated_sprite);
 
 	// Setup the Player
-	Player player(animated_sprite);
+	GameObject &player = Player(player_animated_sprite);
+
+	//Setup NPC AABB
+	c2AABB aabb_npc;
+	aabb_npc.min = c2V(npc.getAnimatedSprite().getPosition().x, npc.getAnimatedSprite().getPosition().y);
+	aabb_npc.max = c2V(
+		npc.getAnimatedSprite().getPosition().x +
+		npc.getAnimatedSprite().getGlobalBounds().width, 
+		npc.getAnimatedSprite().getPosition().y +
+		npc.getAnimatedSprite().getGlobalBounds().height);
+
+	//Setup Player AABB
+	c2AABB aabb_player;
+	aabb_player.min = c2V(player.getAnimatedSprite().getPosition().x, player.getAnimatedSprite().getPosition().y);
+	aabb_player.max = c2V(player.getAnimatedSprite().getGlobalBounds().width / 6, player.getAnimatedSprite().getGlobalBounds().width / 6);
+
+
+	// Initialize Input
 	Input input;
 
 	// Collision result
 	int result = 0;
+
+	// Direction of movement of NPC
+	sf::Vector2f direction(0.1f, 0.2f);
 	
 	// Start the game loop
 	while (window.isOpen())
 	{
 		// Move Sprite Follow Mouse
-		mouse.setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+		player.getAnimatedSprite().setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+		
+		// Move The NPC
+		sf::Vector2f move_to(npc.getAnimatedSprite().getPosition().x + direction.x, npc.getAnimatedSprite().getPosition().y + direction.y);
 
-		// Update mouse AABB
-		aabb_mouse.min = c2V(mouse.getPosition().x, mouse.getPosition().y);
-		aabb_mouse.max = c2V(mouse.getGlobalBounds().width, mouse.getGlobalBounds().width);
+		if (move_to.x < 0) {
+			direction.x *= -1;
+			move_to.x = 0 + npc.getAnimatedSprite().getGlobalBounds().width;
+		}
+		else if (move_to.x + npc.getAnimatedSprite().getGlobalBounds().width >= 800) { 
+			direction.x *= -1;
+			move_to.x = 800 - npc.getAnimatedSprite().getGlobalBounds().width;
+		}
+		else if (move_to.y < 0) { 
+			direction.y *= -1;
+			move_to.y = 0 + npc.getAnimatedSprite().getGlobalBounds().height;
+		}
+		else if (move_to.y + npc.getAnimatedSprite().getGlobalBounds().height >= 600) {
+			direction.y *= -1;
+			move_to.y = 600 - npc.getAnimatedSprite().getGlobalBounds().height;
+		}
+		
+		npc.getAnimatedSprite().setPosition(move_to);
+
+		// Update NPC AABB set x and y
+		aabb_npc.min = c2V(
+			npc.getAnimatedSprite().getPosition().x, 
+			npc.getAnimatedSprite().getPosition().y
+		);
+
+		aabb_npc.max = c2V(
+			npc.getAnimatedSprite().getPosition().x +
+			npc.getAnimatedSprite().getGlobalBounds().width,
+			npc.getAnimatedSprite().getPosition().y +
+			npc.getAnimatedSprite().getGlobalBounds().height
+		);
+
+		// Update Player AABB
+		aabb_player.min = c2V(
+			player.getAnimatedSprite().getPosition().x, 
+			player.getAnimatedSprite().getPosition().y
+		);
+		aabb_player.max = c2V(
+			player.getAnimatedSprite().getPosition().x +
+			player.getAnimatedSprite().getGlobalBounds().width, 
+			player.getAnimatedSprite().getPosition().y + 
+			player.getAnimatedSprite().getGlobalBounds().height
+		);
 
 		// Process events
 		sf::Event event;
@@ -106,8 +167,11 @@ int main()
 		// Update the Player
 		player.update();
 
+		// Update the Player
+		npc.update();
+
 		// Check for collisions
-		result = c2AABBtoAABB(aabb_mouse, aabb_player);
+		result = c2AABBtoAABB(aabb_player, aabb_npc);
 		cout << ((result != 0) ? ("Collision") : "") << endl;
 		if (result){
 			player.getAnimatedSprite().setColor(sf::Color(255,0,0));
@@ -121,7 +185,9 @@ int main()
 
 		// Draw the Players Current Animated Sprite
 		window.draw(player.getAnimatedSprite());
-		window.draw(mouse);
+
+		// Draw the NPC's Current Animated Sprite
+		window.draw(npc.getAnimatedSprite());
 
 		// Update the window
 		window.display();
